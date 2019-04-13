3c745e0c8784d8c57be84435efc8726c98348b42
Alla Doroshkevych <aldor0511@gmail.com>
2019-04-13 18:07:45 -0400

    After merge corrections

--- Web/ClientApp/src/App.js
+++ Web/ClientApp/src/App.js
@@ -12,9 +12,9 @@ import NavMenu from './components/NavMenu'
 import { createStore } from './components/store'
 import { SignIn } from './components/account/SignIn';
 import { SignUp } from './components/account/SignUp';
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-import CalendarSideBar from './components/CalendarSideBar';
 
+import CalendarSideBar from './components/CalendarSideBar';
+const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 class App extends Component {
   static displayName = App.name;
 
@@ -25,18 +25,13 @@ class App extends Component {
 
   render () {
       return (
-          <div>
-            <NavMenu/>
-            <Route exact path='/' render={()=><SideBarLayout sideBarContent={CalendarSideBar} bodyContent={Calendar}/>}/>
-            <Route path='/events' render={()=><SideBarLayout sideBarContent={CalendarSideBar} bodyContent={Events}/>}/>
-            <Route path='/event' component={Event} />
-            <Route path='/chapter' component={Chapter} />
-          </div>
           <BrowserRouter>
               <Switch>
-                  <Route exact path='/' render={(match) => <div><NavMenu /><Calendar {...match} /></div>} />
+                  <Route exact path='/' render={(match)=><div><NavMenu /><SideBarLayout {...match} sideBarContent={CalendarSideBar} bodyContent={Calendar}/></div>}/>
+                  <Route path='/events' render={(match)=><div><NavMenu /><SideBarLayout {...match} sideBarContent={CalendarSideBar} bodyContent={Events}/></div>}/>
                   <Route path='/event:id' render={(match) => <div><NavMenu /><Event {...match} /></div>} />
                   <Route path='/new-event' render={(match) => <div><NavMenu /><Event {...match} /></div>} />
+                  <Route path='/chapter' render={(match) => <div><NavMenu /><Chapter {...match} /></div>} />
                   <Route path='/SignIn' component={SignIn} />
                   <Route path='/SignUp' component={SignUp} />
                   </Switch>
