4ade07fbe1606141b804cc86530d9e05a51c2eed
Dmytro Doroshkevych <dozcent@gmail.com>
2019-04-22 11:20:42 -0400

    filtering data in calendar

--- Web/ClientApp/src/components/Calendar.jsx
+++ Web/ClientApp/src/components/Calendar.jsx
@@ -38,6 +38,7 @@ class Calendar extends Component {
         this.getEventsForMonth = this.getEventsForMonth.bind(this);
         this.setSelectedChapters = this.setSelectedChapters.bind(this);
     }
+
     setSelectedChapters(arr) {
         this.setState({ selectedChapters: arr });
         this.getEventsForMonth(this.state.currentYear, this.state.currentMonth+1, arr);
@@ -56,8 +57,22 @@ class Calendar extends Component {
         if (sites) {
             ids = sites.map(a => a.id);
         }
-        Service.getCalendarEvents(month, year,ids).then(data => component.setState({ events: data }));
+        Service.getCalendarEvents(month+1, year,ids).then(data => component.setState({ events: data }));
     }
+    componentWillReceiveProps(props) {
+        console.log(props);
+        var ids = [];
+        props.dropDownStore.modifiedList.forEach(st => {
+            var tmp = st.chapters.filter(ch => ch.checked);
+            ids = ids.concat(tmp);
+        });
+        if (ids.length != this.state.selectedChapters.length) {
+            this.getEventsForMonth(this.state.currentYear, this.state.currentMonth, ids);
+            this.setState({ selectedChapters:ids });
+        }
+    }
+
+
     componentDidMount(){
       if (this.sideBarRef !== null){
         window.addEventListener("touchstart", (e) => this.startTouch(e), false);
@@ -141,7 +156,7 @@ class Calendar extends Component {
         else {
             this.setState({ calendar: calendar, currentYear: year, currentMonth: month, regularCalendar: true, setFocusTo: -1 });
         }
-        this.getEventsForMonth(year, month + 1, this.state.selectedChapters);
+        this.getEventsForMonth(year, month, this.state.selectedChapters);
     }
 
     incrementMonth(){
