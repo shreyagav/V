305221e99ee5e9440be65cb2fd13bc265019cd67
Dmytro Doroshkevych <ddoroshkevych@hotmail.com>
2021-05-10 08:42:05 -0400

    fix

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -86,12 +86,14 @@ namespace Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
             else
             {
+                app.UseDeveloperExceptionPage();
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
