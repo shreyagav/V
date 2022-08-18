b2a52b23ce2b0fc26b6abdc2c712a19f067d3f4f
Dmytro Doroshkevych <ddoroshkevych@hotmail.com>
2022-08-18 02:43:38 +0000

    Merged PR 5: fixed authorize attribute
    fixed authorize attribute
    
    Related work items: #4

---
+++ Web/AuthorizeByRole.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Models.Context;
+using System.Linq;
+using System.Net;
+
+namespace Web
+{
+    public class AuthorizedRoles : TypeFilterAttribute
+    {
+        public AuthorizedRoles(params string[] authorizedRoles) : base(typeof(AuthorizationFilter))
+        {
+            Arguments = new object[] { authorizedRoles };
+        }
+
+        private class AuthorizationFilter : IActionFilter
+        {
+            private readonly string[] _authorizedRoles;
+            private ApplicationDbContext ctx;
+            public AuthorizationFilter(ApplicationDbContext context, string[] authorizedRoles)
+            {
+                ctx = context;
+                _authorizedRoles = authorizedRoles;
+            }
+
+            public void OnActionExecuting(ActionExecutingContext context)
+            {
+                if(!ctx.AspNetRoles.Any(a => a.Users.Any(b => b.UserName == context.HttpContext.User.Identity.Name) && _authorizedRoles.Contains(a.Name)))
+                {
+                    context.Result = new BadRequestResult();
+                }
+            }
+
+            public void OnActionExecuted(ActionExecutedContext context)
+            {
+            }
+        }
+    }
+}
